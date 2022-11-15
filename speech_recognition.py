2e6eaec34fb750ccf4e9364b865b072c76b14114
puja2504 <114426819+puja2504@users.noreply.github.com>
2022-11-15 20:00:37 +0530

    Add files via upload

---
+++ speech_recognition.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+r=sr.Recognizer()
+with sr.Microphone() as source:
+    print("speak Anything :")
+    audio=r.listen(source,timeout=5,phrase_time_limit=25)
+    print('Done,please wait we are processing what you said...')
+    try:
+        text=r.reongnize_google(audio)
+        print("you said : {}".format(text))
+    except:
+        print("sorry we could not recognize what you said.you can try again...")
